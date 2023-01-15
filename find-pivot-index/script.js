331b2ce16981c297073a191f4915e7625cac8fd8
Matt Wilson <mattwwilson34@gmail.com>
2023-01-14 19:20:37 -0500

    Add: unable to find solution

---
+++ find-pivot-index/script.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+  let sum = nums.reduce((prev, cur) => prev + cur);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log("i", i);
+    console.log("leftSum", leftSum);
+    console.log("rightSum", sum - leftSum - nums[i]);
+
+    if (leftSum === sum - leftSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+  return -1;
+}
+
+console.log(solution([1, 7, 3, 6, 5, 6]));
+
+array = [1, 7, 3, 6, 5, 6];
